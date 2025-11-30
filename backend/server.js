c45d06e5c5d7eca12691388367522dbd29b5e8aa
Saifshr <ssaifssyed17@gmail.com>
2025-11-30 21:07:34 +0530

    imported api for auth

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
 import connectDB from './config/db.js';
-
+import authRoutes from './routes/auth.js'
 // Load env vars
 dotenv.config();
 
@@ -14,6 +14,11 @@ const app = express();
 //middleware
 app.use(cors());
 app.use(express.json())
+
+
+// Routes
+
+app.use('/api/auth',authRoutes);
 
 // test route 
 app.get('/', (req,res)=>{
