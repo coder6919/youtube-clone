85924b66553d53f4a256bca0ba833e38da794e43
Saifshr <ssaifssyed17@gmail.com>
2025-12-03 22:38:34 +0530

    solved minor issues

--- backend/routes/videos.js
+++ backend/routes/videos.js
@@ -1,24 +1,30 @@
 import express from 'express';
 import { protect } from '../middleware/auth.js';
-import { createVideo, deleteVideo, getAllVideos, getVideoById,updateVideo } from '../controllers/video.js';
+import { createVideo, deleteVideo, getAllVideos, getVideoById,updateVideo,likeVideo,dislikeVideo,addView } from '../controllers/video.js';
 
 const router = express.Router();
 
-
+router.put('/:id/view', addView);
 // Define routes
 // POST /api/videos -> Create video (Protected: Must be logged in)
 router.post('/', protect, createVideo)
 
-// GET /api/videos -> Get a;; videos (Public: Anyone can view)
+// GET /api/videos -> Get all videos (Public: Anyone can view)
 router.get('/', getAllVideos)
 
+// Like/Dislike Routes  /api/videos/:id/like  /dislike -> (Protected)
+router.put('/:id/like', protect, likeVideo);
+router.put('/:id/dislike', protect, dislikeVideo);
+
 // GET /api/videos/:id -> Get specific video (Public)
-router.get('/:id', getVideoById)
+router.get('/find/:id', getVideoById);
 
 // DELETE /api/videos/:id -> Delete video (Protected)
 router.delete('/:id', protect, deleteVideo)
 
 // UPDATE /api/videos/:id -> Update video (Protected)
 router.put('/:id', protect, updateVideo);
+
+
 
 export default router
--- backend/server.js
+++ backend/server.js
@@ -19,6 +19,12 @@ const app = express();
 app.use(cors());
 app.use(express.json())
 
+// --- ADD THIS SPY MIDDLEWARE HERE ---
+app.use((req, res, next) => {
+  console.log(`>>> INCOMING REQUEST: ${req.method} ${req.url}`);
+  next();
+});
+
 
 // Routes
 
