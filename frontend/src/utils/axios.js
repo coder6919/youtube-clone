092180a3c7d9d00b8bb482ddb982152182208c8f
Saifshr <ssaifssyed17@gmail.com>
2025-12-04 15:49:06 +0530

    published project to netlify and render

--- frontend/src/utils/axios.js
+++ frontend/src/utils/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: 'http://localhost:5000/api' // connect our express backend
+    baseURL: import.meta.env.VITE_API_URL || 'http://localhost:5000/api' // connect our express backend
 })
 
 // Automatically add token to request header if it exists
